79f3b5ae0dff77bfa7854eced8db16ec04c30f8f
Jazper <jazpersaldana@yahoo.com>
2024-06-09 22:26:38 -0700

    inheritance lab part 1 complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
-
 from user import User
 
 import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name, knowledge=None) -> None:
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+            ]
+        self.knowledge = knowledge
+
     def teach(self):
-        pass
+        random_number = random.randint(0, 7)
+        self.knowledge = self.knowledge[random_number]
+        return self.knowledge
+
+# breakpoint()
